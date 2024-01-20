85f42021af0947671616db958b71c616729d357a
mersive-valerii <vdorokhov@mersive.com>
2024-01-20 12:25:46 +0100

    download license beta

--- src/components/MessageText.tsx
+++ src/components/MessageText.tsx
@@ -1,12 +1,13 @@
 export default function MessageText({
-    text,
-    success,
-    hide
-  }: {
-    text: string;
-    success: boolean;
-    hide: boolean;
-  }) {
-    return <p className={`error-text success-${success} hide-${hide}`}>{text}</p>;
-  }
-  
+  text,
+  success,
+  hide
+}: {
+  text: string;
+  success: boolean;
+  hide: boolean;
+}) {
+  return <div className="message-contatiner">
+    <p className={`error-text success-${success} hide-${hide}`}>{text}</p>
+    </div>
+}
